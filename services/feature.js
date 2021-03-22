e580c581590aacc75654cece7dc86b457a4f0a9b
jponharn <jukkrapong.ponharn@nectec.or.th>
2021-03-22 15:44:08 +0700

    update api

--- services/feature.js
+++ services/feature.js
@@ -1,21 +1,5 @@
 let acc_con = require('../data/access-control.json')
 
-exports.getA = (req, res) => {
-    res.send("A")
-}
-
-exports.getB = (req, res) => {
-    res.send("B")
-}
-
-exports.getC = (req, res) => {
-    res.send("C")
-}
-
-exports.getD = (req, res) => {
-    res.send("D")
-}
-
 exports.getFeatures = (req, res) => {
     let headers = req.headers
     acc = acc_con[headers.role]
